e035072e368108908c26053ebacaace87db19a0c
60936 <studio34pl@gmail.com>
2024-03-05 07:36:54 +0800

    r

--- models/task.js
+++ models/task.js
@@ -1,11 +1,24 @@
 const mongoose = require('mongoose')
 
 const TaskSchema = new mongoose.Schema({
+  unit: {
+    type: Number,
+    required: [true, 'must provide 4 digit unit number'],
+    trim: true,
+  },
   name: {
     type: String,
     required: [true, 'must provide name'],
     trim: true,
     maxlength: [20, 'name can not be more than 20 characters'],
+  },
+  checkin: {
+    type: Date,
+    required: [true, 'must provide yyyy-mm-dd'],
+  },
+  checkout: {
+    type: Date,
+    required: [true, 'must provide yyyy-mm-dd'],
   },
   completed: {
     type: Boolean,
